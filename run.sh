8fe13ad5d961e17efd7cc0bec821541fb365855b
melissaesantos <91588097+melissaesantos@users.noreply.github.com>
2022-02-04 09:57:03 -0800

    fixed 5

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar Hello.java
+javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar Hello.java HelloTester.java
  
 java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore HelloTester
